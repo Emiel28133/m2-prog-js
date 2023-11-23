4ce54d01c087ff99c38583682a7ab7164508a446
Emiel Plokker <28133@ma-web.nl>
2023-11-23 17:39:43 +0100

    commit 2 les 1

--- 01A/app.js
+++ 01A/app.js
@@ -1,9 +1,18 @@
-class App
-{
-    runApplication()
-    {
-       console.log("Hello world");
-       //code gaat hier!
+class App {
+    runApplication() {
+        console.log("Hello world");
+        let appNaam = 'jeroen';
+        let versienummer = 1.2;
+        let auteur = "Emiel";
+        let copyright = "maastricht";
+        let distributor = "henk van veen";
+        let darkmode = true;
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(auteur);
+        console.log(copyright);
+        console.log(distributor);
+        console.log(darkmode);
     }
 }
 
