d1ed7d70881de95046ea5980d30c29f52df4522e
Emiel Plokker <28133@ma-web.nl>
2023-11-29 21:19:59 +0100

    commit 02 03

--- 02 lijnen/app.js
+++ 02 lijnen/app.js
@@ -8,13 +8,10 @@ class App
         let g = canvas.getContext("2d");
 
         g.beginPath()
-        g.fillStyle = "yellow";
         g.moveTo(20,20);
         g.lineTo(300,300);
-        g.lineTo(500,20);
         g.closePath();
         g.stroke();
-        g.fill()
        
         console.log(canvas)
     }
