3609a78ae55a85fa5065fb67b001aba19a9a2d64
Emiel Plokker <28133@ma-web.nl>
2023-12-14 11:57:14 +0100

    0402 arguments

--- 04 functions/app1.js
+++ 04 functions/app1.js
@@ -1,6 +1,16 @@
+function argumentsAreHandy(shoutout)
+{
+    console.log("do u want to give a shoutout?");
+    console.log(shoutout)
+}
+
+argumentsAreHandy()
+argumentsAreHandy("ik ben cool!")
+argumentsAreHandy("wist je dat?")
+
 function globalFunction()
 {
-    console.log("ik ben global dus je ka me altijd aanroepen")
+    console.log("ik ben global dus je ka me altijd aanroepen");
     
 }
 
