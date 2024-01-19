3245d142777e6db597a1062c543cbc5ea99bda56
Emiel Plokker <28133@ma-web.nl>
2024-01-19 12:01:11 +0100

    05 03

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -2,12 +2,8 @@ class App {
     runApplication() {
         console.log("Hello world");
 
-        const artist = ["Slipknot,", "Ghost", "Type o negative", "Marilyn manson", "Sisters of mercy", "christian death", "Killing joke", "bob dylan", "prince"];
-        // console.log(artist);
+        const artist = ["Slipknot,", "Ghost", "Type o negative", "Marilyn manson", "Sisters of mercy", "christian death", "Killing joke", "guns n roses", "led zepplin"];
         const nummers = [2, 5, 7];
-        // console.log(nummers);
-
-        artist.push("guns n roses", "led zepplin")
 
         let indexToRemove = artist.indexOf("bob dylan");
         artist.splice(indexToRemove, 1);
@@ -25,14 +21,28 @@ class App {
             console.log(i + ": " + nummers[i]);
         }
 
+        let headersByClass = document.getElementsByClassName("BandName");
 
+        for (let piss = 0; piss < headersByClass.length; piss++) {
+            
+            console.log(headersByClass[piss].innerText);
+        }
+
+        for (let i = 0; i < headersByClass.length; i++) {
+            headersByClass[i].innerText = "huh????";
+        }
+
+        headersByClass[0].innerText = "niews";
+        headersByClass[1].innerText = "reviews";
+        headersByClass[2].innerText = "commentaar";
+        headersByClass[3].innerText = "Opinions";
+        headersByClass[4].innerText = "Features";
+        headersByClass[5].innerText = "Interviews";
+        headersByClass[6].innerText = "Analysis";
+        headersByClass[7].innerText = "ik wil";
+        headersByClass[8].innerText = "naar huis";
     }
 }
 
 let app = new App();
 app.runApplication();
-
-
-
-
-
