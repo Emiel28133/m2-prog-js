90feac4b6086edbd66336044aff3fd0c89252970
Emiel Plokker <28133@ma-web.nl>
2024-01-21 18:07:16 +0100

    0703

--- 07 classes/app.js
+++ 07 classes/app.js
@@ -2,11 +2,12 @@ class Greet
 {
     constructor()
     {
+        this.greeting = "Greetings!";
     }
 
     showGreeting()
     {
-        console.log("Greetings!");
+        console.log("Greeting van binnen: " +this.greeting);
     }
 }
 
@@ -14,16 +15,20 @@ class GoodBye
 {
     constructor()
     {
+        this.farewell = "Bye Bye!"
     }
 
     showBye()
     {
-        console.log("Bye Bye");
+        console.log("farewell van binnen :" +this.farewell);
     }
 }
 
 let greet = new Greet();
 let bye = new GoodBye();
+
+console.log("Greeting van buiten: " +greet.greeting);
+console.log("farewell van buiten: " +bye.farewell);
 
 greet.showGreeting();
 bye.showBye();
