b120d8278f9b1134921eae663f49c644badecd97
Emiel Plokker <28133@ma-web.nl>
2024-01-21 17:48:43 +0100

    0701

---
+++ 07 classes/app.js
@@ -0,0 +1,20 @@
+class Greet
+{
+    constructor()
+    {
+        console.log("Greetings!");
+    }
+}
+
+class GoodBye
+{
+    constructor()
+    {
+        console.log("Bye Bye");
+    }
+}
+
+let greet = new Greet()
+let bye = new GoodBye()
+
+
