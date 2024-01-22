17e0d8f67fc36f36cf557c1dd801c093f17c4360
Emiel Plokker <28133@ma-web.nl>
2024-01-22 21:49:51 +0100

    0905

--- 09 objecten/app.js
+++ 09 objecten/app.js
@@ -89,6 +89,22 @@ class App {
             console.log(json);
         });
         
+
+
+        fetch("nope.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((json) => {
+            console.log("From the net==============");
+            console.log(json);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
     }
 }
 
