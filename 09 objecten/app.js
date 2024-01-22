1dc02a1d4e9fffa0dcc9d0be3815ba6fccbe7b84
Emiel Plokker <28133@ma-web.nl>
2024-01-22 21:17:04 +0100

    0902

--- 09 objecten/app.js
+++ 09 objecten/app.js
@@ -1,23 +1,63 @@
 class App {
     runApplication() {
         console.log("Hello world");
-        
-        let magicaart= {
+
+        let spotifyPlaylist = {
+            accountName: "leraar",
+            songs: [
+                {
+                    artist: "Slipknot",
+                    title: "Killpop",
+                    album: "The Grey Chapter",
+                    trackNumber: 5,
+                    genre: "Metal"
+                },
+                {
+                    artist: "Sisters of Mercy",
+                    title: "This Corrosion",
+                    album: "Floodland",
+                    trackNumber: 5,
+                    genre: "Gothic Rock"
+                },
+                {
+                    artist: "Bauhaus",
+                    title: "The Sanity Assasin",
+                    album: "Burning From The Inside",
+                    trackNumber: 14,
+                    genre: "Gothic Rock"
+                }
+            ],
+            favoriteArtist: {
+                artistName: "Marilyn Manson",
+                age: 55,
+                grammyCount: 0,
+                number1HitsCount: 0,
+                genre: "rock / hard rock / gothic rock",
+            }
+        };
+        console.log(spotifyPlaylist);
+        console.log("Favorite Artist:", spotifyPlaylist.favoriteArtist);
+
+        for (let i = 0; i < spotifyPlaylist.songs.length; i++) {
+            let song = spotifyPlaylist.songs[i];
+            console.log(`Song ${i + 1}: ${song.title} by ${song.artist}`);
+        }
+
+        let magicaart = {
             attack: 3,
             defense: 2,
             name: "basic creature",
-            cost: ["1R","1B"],
+            cost: ["1R", "1B"],
             text: "this basic creature is better at attacking"
-             
         };
-        let oneCost = magicaart.cost[0]
-        let twoCost = magicaart.cost[1]
+        let oneCost = magicaart.cost[0];
+        let twoCost = magicaart.cost[1];
 
-        console.log("you play: "+magicaart.name);
-        console.log("this basic creature is better at attacking");
-        console.log(`basic creature has ${magicaart.attack} attack`);
-        console.log(`basic creature has ${magicaart.defense} defense`);
-        console.log(`you played: ${oneCost} and ${twoCost}`);
+        console.log("You play: " + magicaart.name);
+        console.log("This basic creature is better at attacking");
+        console.log(`Basic creature has ${magicaart.attack} attack`);
+        console.log(`Basic creature has ${magicaart.defense} defense`);
+        console.log(`You played: ${oneCost} and ${twoCost}`);
     }
 }
 
